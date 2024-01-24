ed8292f3e5ae24b588988859d3e437f664983903
adasneves127 <adasneves127@gmail.com>
2024-01-24 08:55:07 -0500

    202 1/24/24

---
+++ 202/1_24_24/main.py
@@ -0,0 +1,11 @@
+# This is a comment!
+# Python will not look at these lines
+
+print("Hello world!")
+print("This is\nanother print!")
+print("\tThis line is indented!")
+
+print("This is a backslash: \\")
+
+
+
