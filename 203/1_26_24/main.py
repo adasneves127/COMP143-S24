6436f653148d81ac0ba4a31048f94d5819997b79
adasneves127 <adasneves127@gmail.com>
2024-01-26 08:48:30 -0500

    203 1/26/24 Notes

---
+++ 203/1_26_24/main.py
@@ -0,0 +1,19 @@
+print("Hello, world!")
+
+print("Doing some things!")
+
+print("Goodbye world!")
+
+my_int = 10
+print(my_int)
+
+my_float = 1.68
+print(my_float)
+
+print("Hello, world!\nDoing some things!\nGoodbye, world!")
+print("\tThis is indented!")
+
+# name = "Alex", "Dasneves"
+# print(name)
+
+
