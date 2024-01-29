57f5868491f087c9070db0de57e228ba0a6c7a26
adasneves127 <adasneves127@gmail.com>
2024-01-29 08:39:27 -0500

    201 1_29_24

---
+++ 201/1_29_24/main.py
@@ -0,0 +1,28 @@
+# Math Operators
+
+# + (Add)
+# / (Divide)
+# * (Multiply)
+# - (Subtraction)
+# // (Int. Div)
+# % (Mod)
+# ** (Exp.)
+
+print(5 + 7)
+print(15 - 10)
+print(8 * 7)
+print(100 / 7)
+print(100 // 7)
+print(100 % 7)
+print(5 ** 2)
+
+print((5 - 2) * 8)
+
+my_integer = 10
+
+print(my_integer)
+
+
+
+
+
