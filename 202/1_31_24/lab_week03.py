86ce83e3a043123f1dd2ba47953be891acab27a7
adasneves127 <adasneves127@gmail.com>
2024-01-31 08:50:24 -0500

    202 01_31_24

---
+++ 202/1_31_24/lab_week03.py
@@ -0,0 +1,34 @@
+# Explore the empty string ("") and call string methods like upper, lower, title, rstrip, and lstrip on
+# strings using dot notation. Find other string methods by typing the string and the dot in Pycharm and
+# examining the drop-down menu. Call these methods on values ("literals") like "LAura".lower() and on
+# variables (that have values assigned), e.g. first_name.lower().
+
+my_string = "thIS iS a tESt!"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.title())
+
+print("hello WoRlD".upper())
+
+# Work along with your PAL to import the libraries math and random and use them.
+import math
+my_result = math.pow(5, 2)
+print(my_result)
+
+my_sqrt_root = math.sqrt(400)
+print(my_sqrt_root)
+
+import random
+rand_num = random.randint(1, 100)
+print(rand_num)
+
+#  3*3+2 = 11
+#  3*(3+2) = 15
+#  2+3 * 4 = 14
+#  8/4/2 = 1
+#  4/(4/2) = 2
+
+# (22.75*(1 + (0.15)))
+# 22.75*(1 + 0.15)
+
+print(type("Hello!"))
