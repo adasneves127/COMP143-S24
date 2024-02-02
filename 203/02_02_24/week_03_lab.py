db2f2161b5cfc83b4690f62d2566c473f7e4bf42
adasneves127 <adasneves127@gmail.com>
2024-02-02 08:57:05 -0500

    203 2_2_24

---
+++ 203/02_02_24/week_03_lab.py
@@ -0,0 +1,82 @@
+print("")
+
+my_string = "this is a test!"
+# print(type(my_string))
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.title())
+
+print("My name is ALEX!".title())
+
+print(my_string.rstrip("t!"))
+
+data = "\t\t\t\t10.328, Hello!\t\t\n"
+print(data.strip())
+
+import math
+
+print(math.pi)
+print(math.sqrt(25))
+# print(type(math.pi))
+
+import random
+
+print(random.random())
+
+
+
+# 3*3+2
+# 3*(3+2)
+# 2+3 * 4
+# 8/4/2
+# 4/(4/2)
+
+
+# (22.75*(1 + (0.15)))
+# 22.75*(1 + 0.15)
+
+# Assign 1 as the value for the variable a,
+# 2 for the value for the variable b,
+# and -1 for the variable c.
+# Calculate b^2 - 4ac by hand.
+# Now use Python to check your answer.
+
+# Assign 0 as the value for the variable a.
+# Explain what it means to change the value of
+# a from the previous problem, given that a variable
+# references a value. (It is a label for a value.)
+
+# Changing the value of a variable will change what
+# value is referenced after the re-definition.
+
+# What is the remainder when any even number is divided by 2?
+# Illustrate your answer in Python using the  remainder operator
+# (also known as the modulus or mod operator) in various examples.
+
+print(5 % 2)
+print(4 % 2)
+print(7 % 2)
+print(8 % 2)
+print(10 % 2)
+print(1000 % 2)
+
+# Show that the following calculations produce unexpected results and explain why
+# (as explained in the reading assignment):
+#  0.2 + 0.1
+#  3 * 0.1
+
+# Suppose a person purchases three lemons, three limes, and six oranges.
+# Store the number of each fruit in a variable with a descriptive name
+# using appropriate Python style.
+
+num_of_oranges = 6
+num_of_lemons = 3
+num_of_limes = 3
+print(type(num_of_lemons))
+print(type(num_of_limes))
+print(type(num_of_oranges))
+
+
+# For every variable you used in this lab,
+# call the function type on the variable to have Python show the data
+# type of the variable.
